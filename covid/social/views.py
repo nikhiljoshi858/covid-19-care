ee78af24eaa138fc8abae8bb75a7d0b789708324
nikhiljoshi858 <nikhiljoshi858@gmail.com>
2021-03-19 15:10:36 +0530

    birds eye view image integrated with web app

--- covid/social/views.py
+++ covid/social/views.py
@@ -16,62 +16,10 @@ import csv
 import xlwt
 from .models import Video
 
-
-# Create your views here.
-
-FOCAL_LENGTH = 990
-MIN_CONF = 0.3
-NMS_THRESH = 0.3
-MIN_DISTANCE = 180
-PPI = 140
-WIDTH = 41.1
-
-
 global location
 geo_lookup = GeoLookup('776da34f4f37c2fb8f3ad306cc615bff')
 location = geo_lookup.get_own_location()
 location = location['city'] + ', ' + location['region_name'] + ', ' + location['country_name']
-
-
-def detect_people(frame, net, ln, personIdx=0):
-    (H, W) = frame.shape[:2]
-    results = []
-
-    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
-    net.setInput(blob)
-    layerOutputs = net.forward(ln)
-
-    boxes = []
-    centroids = []
-    confidences = []
-
-    for output in layerOutputs:
-        for detection in output:
-            scores = detection[5:]
-            classID = np.argmax(scores)
-            confidence = scores[classID]
-
-            if classID == personIdx and confidence > MIN_CONF:
-                box = detection[0:4] * np.array([W, H, W, H])
-                (centerX, centerY, width, height) = box.astype("int")
-
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
-
-                boxes.append([x, y, int(width), int(height)])
-                centroids.append((centerX, centerY))
-                confidences.append(float(confidence))
-
-    idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
-    if len(idxs) > 0:
-        for i in idxs.flatten():
-            (x, y) = (boxes[i][0], boxes[i][1])
-            (w, h) = (boxes[i][2], boxes[i][3])
-            print('Height, width:',h,w)
-            r = (confidences[i], (x, y, x + w, y + h, ((2*3.14159*180)/(w+h*360)*1000+3)*2.54), centroids[i])
-            results.append(r)
-
-    return results
 
 
 def previous_results_view(request):
@@ -81,6 +29,183 @@ def previous_results_view(request):
     return render(request, 'social/previous.html', context=context)
 
 
+MIN_CONF = 0.5
+NMS_THRESH = 0.5
+mouse_pts = []
+points = []
+mapping = {}
+violate = set()
+centroids = []
+
+
+labelsPath = 'D:/Django_Projects/temp/models/coco.names'
+LABELS = open(labelsPath).read().strip().split("\n")
+weightsPath = 'D:/Django_Projects/temp/models/yolov3.weights'
+configPath = 'D:/Django_Projects/temp/models/yolov3.cfg'
+
+
+def detect_people(frame, net, ln, personIdx=0):
+    (H, W) = frame.shape[:2]
+    results = []
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layerOutputs = net.forward(ln)
+    boxes = []
+    centroids = []
+    confidences = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if classID == personIdx and confidence > MIN_CONF:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                boxes.append([x, y, int(width), int(height)])
+                centroids.append((centerX, centerY))
+                confidences.append(float(confidence))
+
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            r = (confidences[i], (x, y, x + w, y + h), centroids[i])
+            results.append(r)
+
+    return results
+
+
+
+def get_boundary_points(image):
+    cv2.namedWindow("image")
+    cropping = True
+    if cropping:
+        cv2.setMouseCallback("image", get_mouse_points)
+    while True:
+        cv2.imshow("image", image)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("r"):
+            image = clone.copy()
+        elif key == ord("c"):
+            break
+        if len(mouse_pts) <= 4:
+            image = cv2.putText(image, "Enter the boundaries of the Bird's Eye View", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        if len(mouse_pts) > 3 and len(mouse_pts) <= 6:
+            image = cv2.putText(image, "Now, Enter the Threshold distance", (50,75), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        if len(mouse_pts) == 6:
+            cv2.destroyWindow("image")
+            cropping = False
+        if not cropping:
+            break
+
+
+def get_mouse_points(event, x, y, flags, param):
+    global mouse_pts
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if len(mouse_pts) < 4:
+            cv2.circle(image, (x, y), 5, (0, 0, 255), 10)
+        else:
+            cv2.circle(image, (x, y), 5, (255, 0, 0), 10)
+            
+        if len(mouse_pts) >= 1 and len(mouse_pts) <= 3:
+            cv2.line(image, (x, y), (mouse_pts[len(mouse_pts)-1][0], mouse_pts[len(mouse_pts)-1][1]), (70, 70, 70), 2)
+            if len(mouse_pts) == 3:
+                cv2.line(image, (x, y), (mouse_pts[0][0], mouse_pts[0][1]), (70, 70, 70), 2)
+        
+        if "mouse_pts" not in globals():
+            mouse_pts = []
+        mouse_pts.append([x, y])
+
+
+def get_social_distancing_view(image, results, threshold_distance, matrix):
+    for (i, (prob, bbox, centroid)) in enumerate(results):
+        (startX, startY, endX, endY) = bbox
+        (cX, cY) = centroid
+        color = (0, 255, 0)
+        x = startX + ((endX - startX) // 2)
+        y = endY
+        px = int((matrix[0][0]*x + matrix[0][1]*y + matrix[0][2]) // (matrix[2][0]*x + matrix[2][1]*y + matrix[2][2]))
+        py = int((matrix[1][0]*x + matrix[1][1]*y + matrix[1][2]) // (matrix[2][0]*x + matrix[2][1]*y + matrix[2][2]))
+        centroids.append((px, py))
+
+    if len(results) >= 2:
+        D = dist.cdist(centroids, centroids, metric="euclidean").tolist()
+        for i in range(len(D[0])):
+            for j in range(i+1, len(D[0])):
+                if D[i][j] < threshold_distance:
+                    violate.add(i)
+                    violate.add(j)
+
+    for (i, (prob, bbox, centroid)) in enumerate(results):
+        (startX, startY, endX, endY) = bbox
+        (cX, cY) = centroid
+        color = (0, 255, 0)
+        mapping[(i, centroids[i][0], centroids[i][1])] = 'norisk'
+
+        if i in violate:
+            mapping[(i, px, py)] = 'risk'
+            color = (0, 0, 255)
+
+        cv2.rectangle(copied_image, (startX, startY), (endX, endY), color, 2)
+
+    text = "Social Distancing Violations: {}".format(len(violate))
+    cv2.putText(copied_image, text, (10, 25), cv2.FONT_HERSHEY_COMPLEX, 0.85, (0, 0, 255), 3)
+    return copied_image
+
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype = "float32")
+    pts = np.asarray(pts)
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
+
+def four_point_transform(image, pts):
+    rect = order_points(pts)
+    h, w = image.shape[:2]
+    dst = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
+    matrix = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, matrix, (w, h))
+    return matrix, warped
+
+
+def get_transformed_points(boxes, prespective_transform):
+    bottom_points = []
+    for box in boxes:
+        pnts = np.array([[[int(box[0]+(box[2]*0.5)),int(box[1]+box[3])]]] , dtype="float32")
+        bd_pnt = cv2.perspectiveTransform(pnts, prespective_transform)[0][0]
+        pnt = [int(bd_pnt[0]), int(bd_pnt[1])]
+        bottom_points.append(pnt)
+    return bottom_points
+
+
+def get_birds_eye_view(image, mapping):
+    h = image.shape[0]
+    w = image.shape[1]
+
+    red = (0, 0, 255)
+    green = (0, 255, 0)
+    white = (200, 200, 200)
+
+    birds_eye_view = np.zeros((h, w, 3), np.uint8)
+    birds_eye_view[:] = white
+
+    for (i, px, py), risk_factor in mapping.items():
+        if risk_factor == 'risk':
+            cv2.circle(birds_eye_view, (px, py), 5, red, -1)
+        else:
+            cv2.circle(birds_eye_view, (px, py), 5, green, -1)
+    
+    return birds_eye_view
+    
+
 @login_required(redirect_field_name='/social/')
 def homepage_view(request):
     return render(request, 'social/home.html')
@@ -89,64 +214,44 @@ def homepage_view(request):
 @login_required(redirect_field_name='/social/image')
 def image_view(request):
     if request.method == "POST":
-        img = cv2.imdecode(np.fromstring(request.FILES['files'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
+        global image, copied_image
+        image = cv2.imdecode(np.fromstring(request.FILES['files'].read(), np.uint8), cv2.IMREAD_UNCHANGED)
+        copied_image = image.copy()
         
-        labelsPath = 'D:\Django_Projects/temp/models/coco.names'
+        labelsPath = 'D:/Django_Projects/temp/models/coco.names'
         LABELS = open(labelsPath).read().strip().split("\n")
 
-        weightsPath = 'D:\Django_Projects/temp/models/yolov3.weights'
-        configPath = 'D:\Django_Projects/temp/models/yolov3.cfg'
+        weightsPath = 'D:/Django_Projects/temp/models/yolov3.weights'
+        configPath = 'D:/Django_Projects/temp/models/yolov3.cfg'
 
         print("[INFO] loading YOLO from disk...")
         net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
         ln = net.getLayerNames()
         ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        get_boundary_points(image)
+        results = detect_people(image, net, ln, personIdx=0)
 
-        results = detect_people(img, net, ln, personIdx=LABELS.index("person"))
+        threshold_distance = ((mouse_pts[-1][0] - mouse_pts[-2][0]) ** 2 + (mouse_pts[-1][1] - mouse_pts[-2][1]) ** 2) ** 0.5
 
-        violate = set()
+        ordered_points = order_points(mouse_pts[0:4])
+        matrix, warped = four_point_transform(image, ordered_points)
 
-        if len(results) >= 2:
-            centroids = np.array([r[2] for r in results])
-            D = dist.cdist(centroids, centroids, metric="euclidean").tolist()
+        sd_view = get_social_distancing_view(image, results, threshold_distance, matrix)
+        bdv = get_birds_eye_view(warped, mapping) 
 
-            for i in range(0, len(D[0])):
-                for j in range(i + 1, len(D[1])):
-
-                    print(D[i][j])
-                    D[i][j] = D[i][j] * 2.54 / PPI
-                    print('New D:',D[i][j])
-                    print(results[i][1][4], results[j][1][4])
-                    D[i][j] = (D[i][j]**2 + (results[i][1][4] - results[j][1][4])**2) ** 0.5
-                    print('New New D:',D[i][j])
-                    print('\n')
-                    if D[i][j] < MIN_DISTANCE:
-                        violate.add(i)
-                        violate.add(j)
-
-        for (i, (prob, bbox, centroid)) in enumerate(results):
-            (startX, startY, endX, endY, di) = bbox
-            (cX, cY) = centroid
-            color = (0, 255, 0)
-
-            if i in violate:
-                color = (0, 0, 255)
-
-            cv2.rectangle(img, (startX, startY), (endX, endY), color, 2)
-            cv2.circle(img, (cX, cY), 5, color, 1)
-
-        text = "Social Distancing Violations: {}".format(len(violate))
-        cv2.putText(img, text, (10, 25), cv2.FONT_HERSHEY_COMPLEX, 0.85, (0, 0, 255), 3)
-
-        uri = b64encode(cv2.imencode('.jpg', img)[1]).decode()
-        uri = "data:%s;base64,%s" % ("image/jpeg", uri)
+        uri1 = b64encode(cv2.imencode('.jpg', sd_view)[1]).decode()
+        uri1 = "data:%s;base64,%s" % ("image/jpeg", uri1)
+        uri2 = b64encode(cv2.imencode('.jpg', bdv)[1]).decode()
+        uri2 = "data:%s;base64,%s" % ("image/jpeg", uri2)
+        
 
         row = Previous_Social(result=len(violate), category='image', location=location)
         row.save()
 
         context = {}
-        context['image'] = uri
+        context['sd_view'] = uri1
+        context['birds_eye_view'] = uri2
         context['number'] = len(violate)
 
         return render(request, 'social/image_output.html', context)
@@ -173,11 +278,11 @@ def video_view(request):
 
 @login_required(redirect_field_name='/social/webcam')
 def webcam_view(request):
-    labelsPath = 'E:\Django_Projects/temp/models/coco.names'
+    # labelsPath = 'D:/Django_Projects/temp/models/coco.names'
     LABELS = open(labelsPath).read().strip().split("\n")
 
-    weightsPath = 'E:\Django_Projects/temp/models/yolov3.weights'
-    configPath = '/home/nikhil/Desktop/models/yolov3.cfg'
+    # weightsPath = 'E:\Django_Projects/temp/models/yolov3.weights'
+    # configPath = '/home/nikhil/Desktop/models/yolov3.cfg'
 
     print("[INFO] loading YOLO from disk...")
     net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
